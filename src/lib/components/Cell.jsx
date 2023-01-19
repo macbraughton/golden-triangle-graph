14dc05099a7bb10b8eff7a2e5f58a3aadda92044
Donnie <donnie@hyxos.io>
2023-01-19 18:12:02 -0500

    clicking on cell fills it

--- src/lib/components/Cell.jsx
+++ src/lib/components/Cell.jsx
@@ -1,5 +1,5 @@
 import { mergeProps } from 'solid-js'
-import { createSignal, Switch, Match } from 'solid-js'
+import { Switch, Match } from 'solid-js'
 import { h2w, w2h } from '../utils/goldenTriangle'
 const stroke = "#BEBEBE"
 const strokeWidth = 1
@@ -11,11 +11,8 @@ const vbw = h2w(vbh) / 2
 const minX = `-${vbw / 2}`
 const minY = `-${vbh / 2}`
 const viewBox = `${minX} ${minY} ${vbw} ${vbh}`
-const [opacity, setOpacity] = createSignal(1)
-const onClick = () => {
-  if (opacity() === 1) {
-    setOpacity(0.1)
-  } else setOpacity(1)
+const onClick = (e) => {
+  e.target.classList.toggle('no-fill')
 }
 
 const zozo = `M  ${minX} ${minY} L ${-minX} ${minY} L ${-minX} ${-minY} z`
@@ -28,9 +25,10 @@ const WholeCell = (props) => <>
     <path d={props.p} />
   </g>
   <g class="fillcell" stroke="none" fill={props.fill} stroke-width={props.strokeWidth} shape-rendering="geometricPrecision">
-    <path opacity={opacity()} onClick={onClick} d={props.d} />
+    <path onClick={onClick} d={props.d} class="no-fill" />
   </g>
 </>
+
 export default function Cell(props) {
   const merged = mergeProps({ width, height, viewBox, stroke, strokeWidth, fill }, props)
   return <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox={merged.viewBox} width={merged.width} height={merged.height}>
