21c73abd3b757b58b6c475fb2ebebf65bc58b3d6
Donnie <donnie@hyxos.io>
2023-02-27 20:52:50 -0500

    add `rust` and `wasm` to project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import Graph from './lib/components/Graph'
 import ControlPanel from './lib/components/ControlPanel';
 
 function App() {
+  (async () => {
+    let response = await fetch('target/wasm32-unknown-unknown/release/golden_triangle_graph.wasm');
+    let bytes = await response.arrayBuffer();
+    let { instance } = await WebAssembly.instantiate(bytes, { });
+    console.log(instance.exports.add_one(63));
+  })();
   const style = {
     position: "relative", overflow: "hidden"
   }
